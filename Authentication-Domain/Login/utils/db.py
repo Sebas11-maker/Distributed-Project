cd7c39ce9735211cc0e4ef1b06969cf64f6a5342
Sebas11-maker <psbenalcazar@uce.edu.ec>
2026-01-03 20:51:24 -0500

    feat: :bug: fix conection db

--- Authentication-Domain/Login/utils/db.py
+++ Authentication-Domain/Login/utils/db.py
@@ -2,10 +2,12 @@ import pyodbc
 
 def get_connection():
     conn = pyodbc.connect(
-        'DRIVER={ODBC Driver 17 for SQL Server};'
+        'DRIVER={ODBC Driver 18 for SQL Server};'
         'SERVER=database-1.ct8ujriqkfgd.us-east-1.rds.amazonaws.com;'
         'DATABASE=auth_db;'
         'UID=admin;'
-        'PWD=Distribuida123'
+        'PWD=Distribuida123;'
+        'Encrypt=yes;'
+        'TrustServerCertificate=yes;'
     )
     return conn, conn.cursor()
