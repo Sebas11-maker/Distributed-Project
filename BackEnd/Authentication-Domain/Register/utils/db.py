6cb68a74f5eb1872494f6e9b03b1eb17e90f5027
Sebas11-maker <psbenalcazar@uce.edu.ec>
2026-01-23 10:58:52 -0500

    feat: 🐛 fix conection db

--- BackEnd/Authentication-Domain/Register/utils/db.py
+++ BackEnd/Authentication-Domain/Register/utils/db.py
@@ -3,7 +3,7 @@ import pyodbc
 def get_connection():
     conn = pyodbc.connect(
         'DRIVER={ODBC Driver 18 for SQL Server};'
-        'SERVER=98.89.162.195,1433;'
+        'SERVER=54.144.243.21,1433;'
         'DATABASE=auth_db;'
         'UID=sa;'
         'PWD=Password123!;'
