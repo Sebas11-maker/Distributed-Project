180c20782cb878f2d9ca897b1438627a6c07ea77
Sebas11-maker <psbenalcazar@uce.edu.ec>
2026-01-23 01:23:33 -0500

    feat: :art: new structure

--- BackEnd/Authentication-Domain/Login/utils/db.py
+++ BackEnd/Authentication-Domain/Login/utils/db.py
@@ -1,16 +1,13 @@
 import pyodbc
 
 def get_connection():
-    conn_str = (
-        "DRIVER={ODBC Driver 18 for SQL Server};"
-        "SERVER=98.89.162.195,1433;"
-        "DATABASE=auth_db;"
-        "UID=sa;"
-        "PWD=Password123!;"
-        "Encrypt=yes;"
-        "TrustServerCertificate=yes;"
-        "Connection Timeout=30;"
-        "Login Timeout=30;"
+    conn = pyodbc.connect(
+        'DRIVER={ODBC Driver 18 for SQL Server};'
+        'SERVER=98.89.162.195,1433;'
+        'DATABASE=auth_db;'
+        'UID=sa;'
+        'PWD=Password123!;'
+        'Encrypt=yes;'
+        'TrustServerCertificate=yes;'
     )
-    conn = pyodbc.connect(conn_str, timeout=30)
     return conn, conn.cursor()
